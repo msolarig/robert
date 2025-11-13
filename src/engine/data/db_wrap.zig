f4990b77014011881f39688a91b2c740770b54d5
Matias Solari <149914649+msolarig@users.noreply.github.com>
2025-11-13 02:46:42 -0600

    Organized, documented src/engine/data/db_wrap.zig

--- src/engine/data/db_wrap.zig
+++ src/engine/data/db_wrap.zig
@@ -1,29 +1,22 @@
 const std = @import("std");
 
-// ---------------------------------------------------------------------
-// Extern Functions Declaration
-// ---------------------------------------------------------------------
-
+// Extern SQLite3 function declarations -----------------------------------------------------------
 extern fn sqlite3_open(filename: [*:0]const u8, db: *?*anyopaque) c_int;
 extern fn sqlite3_close(db: *anyopaque) c_int;
-pub extern fn sqlite3_prepare_v2(db: *anyopaque, sql: [*:0]const u8, nByte: c_int, 
-                                 stmt: *?*anyopaque, tail: *?*[*:0]const u8) c_int;
+pub extern fn sqlite3_prepare_v2(db: *anyopaque, sql: [*:0]const u8, nByte: c_int, stmt: *?*anyopaque, tail: *?*[*:0]const u8) c_int;
 pub extern fn sqlite3_step(stmt: *anyopaque) c_int;
 pub extern fn sqlite3_finalize(stmt: *anyopaque) c_int;
 pub extern fn sqlite3_column_text(stmt: *anyopaque, col: c_int) [*:0]const u8;
 pub extern fn sqlite3_column_double(stmt: *anyopaque, col: c_int) f64;
 pub extern fn sqlite3_errmsg(db: *anyopaque) [*:0]const u8;
-
-// ---------------------------------------------------------------------
-// Database Interaction File
-// Integration and wraping of SQLite funtions (imported from C modules)
-// ---------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------
 
 const DataBaseError = error{
     FailedToOpen,
     FailedToClose,
 };
 
+/// Open Database - SQLite3 Wrapper 
 pub fn openDB(path: []const u8) !*anyopaque {
   var db_handle: ?*anyopaque = null;
   const c_path = try std.heap.c_allocator.dupeZ(u8, path);
@@ -36,6 +29,7 @@ pub fn openDB(path: []const u8) !*anyopaque {
   return db_handle.?;
 }
 
+/// Close Database - SQLite3 Wrapper 
 pub fn closeDB(db_handle: *anyopaque) !void {
   const close = sqlite3_close(db_handle);
 
